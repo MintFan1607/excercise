a9d7fe57c5446b72652984392a3007b15ece61bc
MintFan1607 <24120384@student.hcmus.edu.vn>
2025-03-26 19:54:21 +0700

    btvn

---
+++ excercise1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+	if (n == 0) { 
+		return 0; // n=0 thì trả về 0 vì là số đầu tiên trong dãy
+	}
+	else if (n == 1) {
+		return 1; // n=1 thì trả về 1 vì là số thứ hai trong dãy
+	}
+	else
+		return fibonacci(n - 2) + fibonacci(n - 1); // n>2 thì trả về tổng của 2 số liền trước
+}
+void solve(int n) {
+	for (int i = 0; i <= n; i++) { // vòng lặp chạy từ i=0 -> i=n
+		cout << fibonacci(i) << " ";
+	}
+}
+
+int main() {
+	int n;
+	cout << "Input: ";
+	cin >> n;
+	cout << "Output: ";
+	solve(n); // trả về kết quả dãy fibonacci kéo dài tới số thứ n trong dãy
+	return 0;
+}
---
+++ excercise3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+void generateBinaryStrings(int n, string str) {
+    
+    if (str.length() == n) { // điều kiện dừng khi độ dài chuỗi str = n
+        cout << str << endl;
+        return;
+    }
+    generateBinaryStrings(n, str + "0");
+    generateBinaryStrings(n, str + "1"); // gọi hàm 2 lần, thêm 0 và 1 vào chuỗi str
+}
+
+int main() {
+    int n;
+    cout << "Input: ";
+    cin >> n;
+
+    cout << "Output: "<<endl;
+    generateBinaryStrings(n, "");
+
+    return 0;
+}
